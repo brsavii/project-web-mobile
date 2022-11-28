d755fa443cb55ef1f1972fc37b2454a3963f87d8
bruna savi <brunasavi47@gmail.com>
2022-11-28 17:41:35 -0300

    primeiro commit

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,16 @@
+import { ImageBackground } from 'react-native';
+import { styles } from './styles';
+import Teste from './../../assets/background-galaxy.png';
+
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground style={styles.container} source={Teste}>
+      {children}
+    </ImageBackground>
+  )
+}
---
+++ mobile/src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {    
+    flex: 1,
+    alignItems: 'center',
+  },
+  logo: {
+    width: 250,
+    height: 145,
+    marginTop: 74,
+    marginBottom: 35
+  },
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64
+  }
+});
